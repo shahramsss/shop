c8d26c58ee097011647488b2df656be4622e4881
shahramsss <shahramsss1987@gmail.com>
2025-07-26 09:30:43 +0330

    add category and product model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=155)
+    slug = models.SlugField(max_length=155, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+        def __str__(self):
+            return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
+    name = models.CharField(max_length=155)
+    slug = models.SlugField(max_length=155, unique=True)
+    image = models.ImageField(upload_to="products/")
+    description = models.TextField()
+    price = models.IntegerField()
+    available = models.BooleanField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ("name",)
+
+        def __str__(self):
+            return self.name
