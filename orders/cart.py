7aaeb7f2a5f70a648fb7c746155ec49fd9a36f73
shahramsss <shahramsss1987@gmail.com>
2025-07-29 10:46:55 +0330

    add cart iter

--- orders/cart.py
+++ orders/cart.py
@@ -1,3 +1,5 @@
+from home.models import Product
+
 CART_SESSION_ID = "cart"
 
 
@@ -8,6 +10,17 @@ class Cart:
         if not cart:
             cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]["product"] = product
+
+        for item in cart.values():
+            item["total_price"] = int(item["price"]) * item["quantity"]
+            yield item
 
     def add(self, product, quantity):
         product_id = product.id
