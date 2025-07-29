34c1138ea6a7b3ca77f7ffd911aa787e115a73d4
shahramsss <shahramsss1987@gmail.com>
2025-07-29 11:40:41 +0330

    add remoce_cart item

--- orders/cart.py
+++ orders/cart.py
@@ -16,10 +16,10 @@ class Cart:
         products = Product.objects.filter(id__in=product_ids)
         cart = self.cart.copy()
         for product in products:
-            cart[str(product.id)]['product'] = product
+            cart[str(product.id)]["product"] = product
 
         for item in cart.values():
-            item['total_price'] = int(item['price']) * item['quantity']
+            item["total_price"] = int(item["price"]) * item["quantity"]
             yield item
 
     def add(self, product, quantity):
@@ -29,5 +29,14 @@ class Cart:
         self.cart[product_id]["quantity"] += quantity
         self.save()
 
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
     def save(self):
         self.session.modified = True
+
+    def get_total_price(self):
+        return sum(int(item["price"]) * item["quantity"] for item in self.cart.values())
