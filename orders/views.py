bf523419a4a6cbbf63b87f5a2e7e93bc75107703
shahramsss <shahramsss1987@gmail.com>
2025-07-29 10:28:12 +0330

    add CartAddView

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect , get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from home.models import Product
 from .cart import Cart
-
+from .forms import CartAddForm
 
 
 class CartView(View):
@@ -12,4 +12,9 @@ class CartView(View):
 
 class CartAddView(View):
     def post(self, request, product_id):
-        pass
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data["quantity  "])
+        return redirect("orders:cart")
